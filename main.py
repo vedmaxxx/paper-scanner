64b7a1e43d18bc75f60bc3afc02455738bafa32e
Vildan Mustafin <vild.mustafin2017@yandex.ru>
2025-11-30 19:23:21 +0500

    SomeChanges:)

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-import os
+# 
+# import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # Использовать первую GPU
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # Использовать первую GPU
 
-from deeppavlov import build_model, configs
+# from deeppavlov import build_model, configs
+# from deeppavlov.core.commands.utils import parse_config
+# import numpy as np
+
+# from train import train
+# from utils import load_texts_from_txt
+
+from deeppavlov import train_model
 from deeppavlov.core.commands.utils import parse_config
-import numpy as np
 
-from train import train
-from utils import load_texts_from_txt
+config = parse_config('config.json')
+config['dataset_reader']['data_path'] = './dataset'
 
+model = train_model(config)
 
 # НУЖНО ПОМЕНЯТЬ ПУТЬ К КОНФИГУ ДАТАСЕТА
 # https://docs.deeppavlov.ai/en/master/features/models/classification.html#6.-Train-the-model-on-your-data
-config = configs.classifiers.topics_distilbert_base_uncased
-print(parse_config("topics_distilbert_base_uncased")["dataset_reader"]["data_path"])
+# config = configs.classifiers.topics_distilbert_base_uncased
+# print(parse_config("topics_distilbert_base_uncased")["dataset_reader"]["data_path"])
 
 
-# model = build_model(config, download=False, install=False)
+# # model = build_model(config, download=False, install=False)
 
-train()
+# train()
 
 # Чтение текстов из TXT-файла(ов)
 # Можно передать один файл:
